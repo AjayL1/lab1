594bd745f7d4ffb074e171c44d7de92256a8b8fd
AjayL1 <92713339+AjayL1@users.noreply.github.com>
2021-10-25 20:38:37 +0530

    Update Fibonacci.java

--- src/lab1/Fibonacci.java
+++ src/lab1/Fibonacci.java
@@ -1,14 +1,15 @@
 package lab1;
-import java.util.Scanner;
+import java.util.Scanner;//imports scanner object class property
 public class Fibonacci {
 		static int recursive(int n) {
 			if(n==1) return 1;
 			if(n==0) return 0;
 			return recursive(n-1)+ recursive(n-2);
-		}
+		}//method where we write the logic
 		static int nonRecursive(int n) {
 			int a=1,b=1,c;
 			int count=2;
+			//recursion is happening here
 			while(count<n) {
 				count++;
 				c=a+b;
@@ -18,9 +19,9 @@ public class Fibonacci {
 			return b;
 		}
 		public static void main(String[]args) {
-		Scanner sc= new Scanner(System.in);
+		Scanner sc= new Scanner(System.in);//to read the user input
 		System.out.print("enter a number");
-		int n=sc.nextInt();
+		int n=sc.nextInt//to store the input in a variable
 
 		System.out.println(recursive(n));
 		System.out.println(nonRecursive(n));
