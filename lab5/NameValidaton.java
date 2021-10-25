23822f11adb5ffd5617a543630236885665bb740
AjayL1 <92713339+AjayL1@users.noreply.github.com>
2021-10-25 14:22:28 +0530

    Update NameValidaton.java

--- lab5/NameValidaton.java
+++ lab5/NameValidaton.java
@@ -1,15 +1,16 @@
 package lab5;
-import java.util.*;
+import java.util.*;// imports scanner class property
 class InvalidNameException extends Exception{
 }
 public class NameValidaton {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc=new Scanner(System.in);// reads the input given by the user
 		System.out.println("first name");
-		String firstname=sc.next();
+		String firstname=sc.next();// store the input given by the user
 		System.out.println("last name");
 		String lastname=sc.next();
+		//to check the input given by the user
 		try {
 			if(firstname.isEmpty() || lastname.isEmpty()) {
 				throw new InvalidNameException();
@@ -18,7 +19,7 @@ public class NameValidaton {
 		}catch(InvalidNameException v) {
 			System.out.println("Firstname and lastName cannot be blank");
 		}finally {
-			System.out.println("your name is : "+ firstname +" "+ lastname);
+			System.out.println("your name is : "+ firstname +" "+ lastname);//to print the output
 			
 		}
 
