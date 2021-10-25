454e20e6faad29c63659019108bf03c905a7aff5
AjayL1 <92713339+AjayL1@users.noreply.github.com>
2021-10-25 21:07:41 +0530

    Update Lab1Ex5.java

--- src/lab1/Lab1Ex5.java
+++ src/lab1/Lab1Ex5.java
@@ -1,10 +1,11 @@
 package lab1;
-import java.util.*;
+import java.util.*;//to import scanner class property
 public class Lab1Ex5 {
 
 	 int s=0;
    public int calculateSum(int n)
    {
+	   //loop to calculate the sum of the given input by the user
           for(int i=0;i<=n;i++)
           {
                  if(i%3==0||i%5==0)
@@ -16,7 +17,6 @@ public class Lab1Ex5 {
    }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		    	 Lab1Ex5 p1 = new Lab1Ex5();
 		           Scanner s = new Scanner(System.in);
 		           int n=s.nextInt();
